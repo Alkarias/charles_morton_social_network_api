ce4bd1fa945164e49bfcdcb3940a86490e05a259
Alkarias <alkarias29@gmail.com>
2022-08-24 14:44:40 -0400

    added thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,26 @@
 const { Thought } = require('../models');
 
 module.exports = {
-    
+    async getAllThoughts() {
+        const thoughtData = await Thought.find();
+        res.status(200).json(thoughtData);
+    },
+    async getSingleThought() {
+
+    },
+    async createThought() {
+
+    },
+    async updateThought() {
+
+    },
+    async deleteThought() {
+
+    },
+    async addReaction() {
+
+    },
+    async deleteReaction() {
+
+    },
 }
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction 
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:id/reactions').post(addReaction).delete(deleteReaction);
+
+module.exports = router;
