f6b3d6ed6b6d567010ede2b28a0a4793b599213b
Alkarias <alkarias29@gmail.com>
2022-08-24 13:44:50 -0400

    updated user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,31 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     async getAllUsers(req, res) {
-        const users = await User.find({})
-        res.status(200).json(users);
+        const userData = await User.find({})
+        .populate('thoughts')
+        .populate('friends');
+        res.status(200).json(userData);
     },
-    async createUser(req, res) {
-
+    async createUser(req, res) { 
+        const userData = await User.create(req.body);
+        res.status(200).json(userData);
     },
-    async getSingleUser(req, res) {
-
+    async getSingleUser(req, res) { 
+        const userData = await User.findOne({ _id: req.params.id })
+        .populate('thoughts')
+        .populate('friends');
+        res.status(200).json(userData);
     },
     async updateUser(req, res) {
-
+        const userData = await User.findOneAndUpdate({ _id: req.params.id })
+        .populate('thoughts')
+        .populate('friends');
+        res.status(200).json(userData);
     },
     async deleteUser(req, res) {
-
+        const userData = await User.findOneAndDelete({ _id: req.params.id })
+        .populate('thoughts')
+        .populate('friends');
+        res.status(200).json(userData);
     },
 };
