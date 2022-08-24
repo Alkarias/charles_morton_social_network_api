fdfac612851bee5fe4de73ef545484210154476b
Alkarias <alkarias29@gmail.com>
2022-08-24 11:52:54 -0400

    built thought model

--- models/user.js
+++ models/user.js
@@ -20,13 +20,13 @@ const userSchema = new Schema(
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'Thought'
+                ref: 'thought'
             }
         ],
         friends: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'User'
+                ref: 'user'
             }
         ]
     },
@@ -34,13 +34,14 @@ const userSchema = new Schema(
         toJSON: {
             virtuals: true,
         }
-    }
+    },
+    id: false
 );
 
 userSchema.virtual('friendCount').get(function() {
     return this.friends.length;
 });
 
-const User = model('User', userSchema);
+const User = model('user', userSchema);
 
 module.exports = User;
