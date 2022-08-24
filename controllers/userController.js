9b12d8d29c4df1d152420dc19f39fa480be0f3ac
Alkarias <alkarias29@gmail.com>
2022-08-24 14:35:33 -0400

    updated user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 
 module.exports = {
     async getAllUsers(req, res) {
@@ -12,21 +12,54 @@ module.exports = {
         res.status(200).json(userData);
     },
     async getSingleUser(req, res) { 
-        const userData = await User.findOne({ _id: req.params.id })
+        const userData = await User.findById(req.params.id)
         .populate('thoughts')
         .populate('friends');
         res.status(200).json(userData);
     },
     async updateUser(req, res) {
-        const userData = await User.findOneAndUpdate({ _id: req.params.id })
-        .populate('thoughts')
+        const userData = await User.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true }
+        ).populate('thoughts')
         .populate('friends');
         res.status(200).json(userData);
     },
     async deleteUser(req, res) {
-        const userData = await User.findOneAndDelete({ _id: req.params.id })
-        .populate('thoughts')
-        .populate('friends');
+        const userData = await User.findByIdAndDelete(req.params.id);
         res.status(200).json(userData);
     },
+    async addFriend(req, res) {
+        // add a new friend to the user's friends list
+        const userData = await User.findByIdAndUpdate(
+            req.params.userId,
+            {$push: {friends: req.params.friendId}},
+            { new: true }
+        ).populate('friends');
+        // add the user to the friend's friends list as well
+        await User.findByIdAndUpdate(
+            req.params.friendId,
+            {$push: {friends: req.params.userId}},
+            { new: true }
+        ).populate('friends');
+        // return the updated data
+        res.status(200).json(userData);
+    },
+    async removeFriend(req, res) {
+        // delete a friend
+        const userData = await User.findByIdAndUpdate(
+            req.params.userId,
+            {$pull: {friends: req.params.friendId}},
+            { new: true }
+        ).populate('friends');
+        // remove the user from the friend's list as well
+        await User.findByIdAndUpdate(
+            req.params.friendId,
+            {$pull: {friends: req.params.userId}},
+            { new: true }
+        ).populate('friends');
+        // return the updated data
+        res.status(200).json(userData);
+    }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,11 +4,15 @@ const {
     getAllUsers,
     getSingleUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    removeFriend
 } = require('../../controllers/userController.js');
 
 router.route('/').get(getAllUsers).post(createUser);
 
 router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
